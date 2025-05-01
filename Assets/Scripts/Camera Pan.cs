310f7037d1a12b66c98cddae9a4b4fdc801937ce
Muscheid1 <114819154+Muscheid1@users.noreply.github.com>
2025-05-01 13:51:40 -0500

    Safety push

--- Assets/Scripts/Camera Pan.cs
+++ Assets/Scripts/Camera Pan.cs
@@ -24,7 +24,7 @@ public class CameraPan : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //return; //for debugging
+        //return;
         if (!gameOver && bunnyState.bunnyOverLeft)
         {
             StartCoroutine(BunnyOver(320f));
